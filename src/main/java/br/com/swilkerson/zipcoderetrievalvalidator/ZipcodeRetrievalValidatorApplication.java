9cf795cf1d2e1b50d9a06501d4bfd3bd75a97a85
S. Wilkerson <shelcicky@yahoo.com.br>
2024-07-04 20:58:23 -0300

    [001]: Added required files;

---
+++ src/main/java/br/com/swilkerson/zipcoderetrievalvalidator/ZipcodeRetrievalValidatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.swilkerson.zipcoderetrievalvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodeRetrievalValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodeRetrievalValidatorApplication.class, args);
+	}
+
+}
