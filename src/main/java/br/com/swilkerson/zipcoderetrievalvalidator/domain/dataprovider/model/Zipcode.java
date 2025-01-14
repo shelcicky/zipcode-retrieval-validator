c8732ee928fa13030b76f46c4fc12703e35079e9
S. Wilkerson <shelcicky@yahoo.com.br>
2025-01-14 12:33:03 -0300

    [010]: Renamed Zipcode response attributes names to english;

--- src/main/java/br/com/swilkerson/zipcoderetrievalvalidator/domain/dataprovider/model/Zipcode.java
+++ src/main/java/br/com/swilkerson/zipcoderetrievalvalidator/domain/dataprovider/model/Zipcode.java
@@ -1,14 +1,15 @@
 package br.com.swilkerson.zipcoderetrievalvalidator.domain.dataprovider.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Zipcode(@JsonProperty("cep") String zipcode,
-                      @JsonProperty("logradouro") String publicPlace,
-                      @JsonProperty("complemento") String complement,
-                      @JsonProperty("unidade") String unit,
-                      @JsonProperty("bairro") String neighborhood,
-                      @JsonProperty("localidade") String city,
-                      @JsonProperty("uf") String state) {
+public record Zipcode(@JsonAlias("cep") @JsonProperty("zip_code") String zipcode,
+                      @JsonAlias("logradouro") @JsonProperty("street") String publicPlace,
+                      @JsonAlias("complemento") @JsonProperty("complement") String complement,
+                      @JsonAlias("unidade") @JsonProperty("unit") String unit,
+                      @JsonAlias("bairro") @JsonProperty("neighborhood") String neighborhood,
+                      @JsonAlias("localidade") @JsonProperty("city") String city,
+                      @JsonAlias("uf") @JsonProperty("state") String state) {
 }
